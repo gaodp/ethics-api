3c122e229d6fa5d984de0f0f78b5d3a045e09807
Matt Farmer <matt@frmr.me>
2015-02-24 19:47:56 -0500

    Switch candidate and report subobjects to use Object IDs.

--- seed/campaigns.js
+++ seed/campaigns.js
@@ -4,14 +4,14 @@ db = conn.getDB("ethics-api");
 for(var i = 1; i <= 3; i ++) {
   var json = {
     candidate: {
-      _id: i.toString(),
+      _id: new ObjectId(),
       lastName: "lastName_" + i,
       firstName: "firstName_" + i
     },
     office: "office_" + i,
     status: "active",
     reports: [{
-      _id: i.toString(),
+      _id: new ObjectId(),
       type: "type_" + i,
       year: new Date().getFullYear(),
       title: "title_" + i,
